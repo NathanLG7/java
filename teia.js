996203c0bb3709e9ca917c217e116cb14f5030c3
NathanLG7 <143437707+NathanLG7@users.noreply.github.com>
2024-03-12 14:34:30 -0300

    Add files via upload

---
+++ teia.js
@@ -0,0 +1,15 @@
+const texto1 = "ola, mundo";
+const texto2 = "ola, mundo";
+const senha = "senha";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Nathan_LG";
+
+// concatenaçao (+)
+
+console. log(citacao + meuNome)
+
+//template string ou temlate literal
+
+console.log(citacao + meuNome)
---
+++ type-number.js
@@ -0,0 +1,23 @@
+// 1º
+
+const meuNumro = 3;
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+const operacaoMatematica =  primeiroNumero + segundoNumero;
+
+console. log(operacaoMatematica) 
+
+// 2º
+const x = 3.3;
+const y = .5;
+
+const z = primeiroNumero / y;
+
+console. log(z)
+
+// 3º
+
+const alura = "alura"
+console. log(alura *primeiroNumero)
